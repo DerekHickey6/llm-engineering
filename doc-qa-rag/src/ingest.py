9b1aab07ac3c1119b690176af67e652371356873
Derek Hickey <derekhickey7@gmail.com>
2026-03-09 13:38:40 -0700

    complete store_in_chromadb + convenience functions

--- doc-qa-rag/src/ingest.py
+++ doc-qa-rag/src/ingest.py
@@ -2,8 +2,10 @@ import chromadb
 from pypdf import PdfReader
 import ollama
 
+datamining_path = "C:\\Users\\derek\\Desktop\\llm-engineering\\doc-qa-rag\\data\\Intro-to-Data-Mining.pdf"
+
 # Load the pdf
-def ingest(pdf_path, collection_name="datamining"):
+def ingest(pdf_path):
     pdf = PdfReader(pdf_path)
     page_text = []
 
@@ -34,11 +36,27 @@ def embed_chunks(chunks):
 
     return embeddings
 
+# Store in persistant chromaDB locally
+def store_in_chromadb(chunks, embeddings, collection_name="datamining"):
+    persistant_client = chromadb.PersistentClient(path="./chroma_db")
+    collection = persistant_client.get_or_create_collection(name=collection_name)
+
+    collection.add(documents=chunks,
+                   embeddings=embeddings,
+                   ids=[f"chunk_{i}" for i in range(len(chunks))])
+
+# Convenience method for running all ingest functions
+def run_ingest(pdf_path):
+    chunks = chunk_text(ingest(pdf_path))
+    store_in_chromadb(chunks=chunks, embeddings=embed_chunks(chunks))
 
 # Test
 if __name__ == "__main__":
-    text = ingest("C:\\Users\\derek\\Desktop\\llm-engineering\\doc-qa-rag\\data\\Intro-to-Data-Mining.pdf")
-    chunked_text = chunk_text(text)
-    embeddings = embed_chunks(chunked_text)
-    print(len(embeddings))
-    print(len(embeddings[0]))
+    run_ingest(datamining_path)
+
+    # text = ingest("C:\\Users\\derek\\Desktop\\llm-engineering\\doc-qa-rag\\data\\Intro-to-Data-Mining.pdf")
+    # chunked_text = chunk_text(text)
+    # embeddings = embed_chunks(chunked_text)
+    # store_in_chromadb(chunked_text, embeddings)
+    # print(len(embeddings))
+    # print(len(embeddings[0]))
