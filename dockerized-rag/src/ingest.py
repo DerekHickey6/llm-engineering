9358083a04142875d2cf224b45a9c80e1bd47f4d
Derek Hickey <derekhickey7@gmail.com>
2026-03-12 12:20:43 -0700

    complete ingest.py

---
+++ dockerized-rag/src/ingest.py
@@ -0,0 +1,50 @@
+import chromadb
+from pypdf import PdfReader
+import ollama
+
+# Ingest -> Chunk -> Embed -> Store
+
+def ingest(pdfpath):
+    reader = PdfReader(pdfpath)
+    extracted_text = []
+
+    for page in reader.pages:
+        text = page.extract_text()
+        if text is not None:
+            extracted_text.append(text)
+
+    extracted_text = "".join(extracted_text)
+    return extracted_text
+
+
+def chunk_text(text, len_chunks=500, offset=50):
+    chunks = []
+
+    for i in range(0, len(text)//len_chunks, offset):
+        chunks.append(text[i:i+len_chunks])
+
+    return chunks
+
+
+def embed_chunks(chunks):
+    embeddings = []
+    for chunk in chunks:
+        embeddings.append(ollama.embeddings(model='nomic-embed-text', prompt=chunk)['embedding'])
+
+    return embeddings
+
+
+def create_db(chunks, embeddings, collection='datamining'):
+    client = chromadb.PersistentClient('/chroma_db')
+    collection = client.get_or_create_collection(name=collection)
+
+    collection.add(
+        documents=chunks,
+        embeddings=embeddings,
+        ids=[f"Chunk_{i}" for i in range(len(chunks))]
+    )
+
+def run_ingest(pdfpath):
+    chunks = chunk_text(ingest(pdfpath))
+
+    return create_db(chunks, embed_chunks(chunks))
